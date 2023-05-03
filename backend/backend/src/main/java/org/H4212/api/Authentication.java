e194afbe6c8468bffff1f13405d4b779a7ade8d0
molivier1 <MaloOLIVIER333@gmail.com>
2023-05-03 08:39:03 +0200

    adds api implementation | BEGINNING

---
+++ backend/backend/src/main/java/org/H4212/api/Authentication.java
@@ -0,0 +1,26 @@
+package org.H4212.api;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+@Path("/api/")
+@Api(description = "the authentication API")
+public class Authentication {
+
+    @POST
+    @Path("/auth/token")
+    @Consumes({ "application/json", "application/x-www-form-urlencoded" })
+    @Produces({ "application/json" })
+    @ApiOperation(value = "Authenticates user and generates an access token", notes = "Validate credentials of a user and, if available, get an access token. ", tags={ "Authentication" })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "An access token was generated successfully. ", response = GetAccessTokenResponse.class),
+            @ApiResponse(code = 400, message = "Bad request", response = GetErrorResponse.class),
+            @ApiResponse(code = 429, message = "Too Many requests", response = GetErrorResponse.class) })
+    Response createAccessToken(@Valid @NotNull GetAccessTokenRequest getAccessTokenRequest){
+
+    }
+
+
+}
