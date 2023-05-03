4413bcbcbf6af9e2cbb0f2a911e8a717d1b3878b
molivier1 <MaloOLIVIER333@gmail.com>
2023-05-03 10:19:40 +0200

    une vraie galère laisse tomber

---
+++ back-end/prescrypt/src/main/sqlfiles/CreatePrescrypt.sql
@@ -0,0 +1,18 @@
+CREATE TABLE doctor
+(
+    Id bigint NOT NULL
+)
+
+--do not persist patients and prescriptions because of RGPD
+
+CREATE TABLE pharmacist
+(
+
+)
+
+CREATE TABLE medication
+(
+
+)
+
+
