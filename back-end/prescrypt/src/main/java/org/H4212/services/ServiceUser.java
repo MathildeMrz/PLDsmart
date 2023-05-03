a7dbe7c41e9c8007d75b6f4d01d1c25216a46a07
molivier1 <MaloOLIVIER333@gmail.com>
2023-05-03 12:10:34 +0200

    small corrections

--- back-end/prescrypt/src/main/java/org/H4212/services/ServiceUser.java
+++ back-end/prescrypt/src/main/java/org/H4212/services/ServiceUser.java
@@ -2,10 +2,26 @@ package org.H4212.services;
 
 import org.H4212.entities.*;
 
+import javax.inject.Inject;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import java.util.List;
+
 public class ServiceUser {
+
+    @Inject Session session;
 
     public Person authenticate(String username, String password)
     {
+        String stringQuery =
+                """
+                    SELECT u from user where u.username = :username AND u.password = :password;
+                """;
+        Query query = session.createNativeQuery(stringQuery);
+        query.setParameter("username",username);
+        query.setParameter("password", password);
+        List resultSet = query.getResultList();
 
+        return (Person) resultSet.get(0);
     }
 }
