5507d431b04ed227402c206ecff593cd95c2c431
molivier1 <MaloOLIVIER333@gmail.com>
2023-05-03 19:22:28 +0200

    tries authentication

--- back-end/prescrypt/src/main/java/org/H4212/api/jsonSerializers/AuthenticateUserResponse.java
+++ back-end/prescrypt/src/main/java/org/H4212/api/jsonSerializers/AuthenticateUserResponse.java
@@ -1,30 +1,22 @@
 package org.H4212.api.jsonSerializers;
 
+import jakarta.json.Json;
+import jakarta.json.JsonBuilderFactory;
+import jakarta.json.JsonObject;
 import org.H4212.entities.Person;
 
 import jakarta.validation.*;
 import java.io.Serializable;
 
-public class AuthenticateUserResponse implements Serializable {
-
-    private @Valid boolean authenticated = false;
+public class AuthenticateUserResponse {
 
     private @Valid Person person;
 
-    public AuthenticateUserResponse(boolean authenticated, Person user) {
-        this.authenticated = authenticated;
+    public AuthenticateUserResponse(Person user) {
         this.person = user;
     }
 
     public AuthenticateUserResponse() {
-    }
-
-    public boolean isAuthenticated() {
-        return authenticated;
-    }
-
-    public void setAuthenticated(boolean authenticated) {
-        this.authenticated = authenticated;
     }
 
     public Person getPerson(){
@@ -33,5 +25,12 @@ public class AuthenticateUserResponse implements Serializable {
 
     public void setUser(Person user){
         this.person = user;
+    }
+
+    public JsonObject toJson()
+    {
+        return person.toJson();
+
+
     }
 }
--- back-end/prescrypt/src/main/java/org/H4212/services/ServiceUser.java
+++ back-end/prescrypt/src/main/java/org/H4212/services/ServiceUser.java
@@ -2,21 +2,24 @@ package org.H4212.services;
 
 import org.H4212.entities.Person;
 
+import jakarta.inject.Inject;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
 public class ServiceUser {
 
-
-
+    @Inject Session session;
     public Person authenticate(String username, String password)
     {
         String stringQuery =
                 """
                     SELECT u from user where u.username = :username AND u.password = :password;
                 """;
-        /*Query query = session.createNativeQuery(stringQuery);
-        query.setParameter("username",username);
-        query.setParameter("password", password);
+        Query query = session.createNativeQuery(stringQuery);
         List resultSet = query.getResultList();
+        
 
-        return (Person) resultSet.get(0);*/return null;
     }
 }
