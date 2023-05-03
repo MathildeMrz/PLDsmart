3a5d6b17489e872df4341e2012e94718c2be2ebf
Xynaanthrok <korantin.aime@insa-lyon.fr>
2023-05-03 18:35:33 +0200

    🚚 Created multi-page app

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  pages: {
+    'index': {
+      entry: 'src/pages/home/main.js',
+      template: 'public/index.html',
+      title: 'Home Page',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    'prescription': {
+      entry: 'src/pages/prescription/main.js',
+      template: 'public/prescription.html',
+      title: 'Prescription Page',
+      chunks: ['chunk-vendors', 'chunk-common', 'prescription']
+    }
+  }
+}
