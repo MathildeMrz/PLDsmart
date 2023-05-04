210ee10ea45d28299561cab1fbe75860e2a0d8f6
molivier1 <MaloOLIVIER333@gmail.com>
2023-05-04 10:37:59 +0200

    authentication API almost over

--- back-end/prescrypt/src/main/java/org/H4212/services/ServiceUser.java
+++ back-end/prescrypt/src/main/java/org/H4212/services/ServiceUser.java
@@ -1,25 +1,52 @@
 package org.H4212.services;
 
-import org.H4212.entities.Person;
+import org.H4212.entities.*;
 
 import jakarta.inject.Inject;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 
-import java.util.List;
+import java.sql.*;
+import java.util.*;
 
 public class ServiceUser {
 
-    @Inject Session session;
-    public Person authenticate(String username, String password)
+    String URL = "jdbc:postgresql://localhost:5432/prescrypt";
+    String USER = "prescrypt";
+    String PASSWORD = "prescrypt";
+    Connection connection;
     {
+        try {
+            DriverManager.registerDriver(new org.postgresql.Driver());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    {
+        try {
+            connection = DriverManager.getConnection(URL,USER,PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public Person authenticate(String username, String password) throws SQLException {
         String stringQuery =
                 """
-                    SELECT u from user where u.username = :username AND u.password = :password;
+                    SELECT id, username, password from users WHERE username = ? AND password = ?;
                 """;
-        Query query = session.createNativeQuery(stringQuery);
-        List resultSet = query.getResultList();
-        
 
+        PreparedStatement preparedStatement = connection.prepareStatement(stringQuery) ;
+        preparedStatement.setString(1, username);
+        preparedStatement.setString(2, password);
+
+        ResultSet resultSet;
+        try {
+            resultSet = preparedStatement.executeQuery();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new Person();
     }
 }
