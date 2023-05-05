c8f0d0bc095f50fb0c9c1fb7776cccef832151b7
Xynaanthrok <korantin.aime@insa-lyon.fr>
2023-05-05 08:59:00 +0200

    ✨ Created Medicament component

--- frontend/src/components/DoctorPrescription.vue
+++ frontend/src/components/DoctorPrescription.vue
@@ -1,153 +1,122 @@
 <template>
-    <body>
-        <nav  class="navigation-bar">
-            <img class="logo" src="../assets/entete.png">
-        </nav>
-    
-        <div id="doctorPrescription">
+    <nav  class="navigation-bar">
+        <img class="logo" src="../assets/entete.png">
+    </nav>
 
-            <div id="doctor">
-                <div>
-                    <h3>Le médecin</h3>
+    <div id="doctorPrescription">
+
+        <div id="doctor">
+            <div>
+                <h3>Le médecin</h3>
+            </div>
+            <div class="information">
+                <div class="column">
+                    <p id="doctorName">Docteur DUBOIS Charline</p>
+                    <p class="indications">NOM Prénom</p>
                 </div>
-                <div class="information">
-                    <div class="column">
-                        <p id="doctorName">Docteur DUBOIS Charline</p>
-                        <p class="indications">NOM Prénom</p>
-                    </div>
 
-                    <div class="column">
-                        <p id="doctorJob">Médecin généraliste</p>
-                        <p class="indications">Qualification</p>
-                    </div>
+                <div class="column">
+                    <p id="doctorJob">Médecin généraliste</p>
+                    <p class="indications">Qualification</p>
+                </div>
 
-                    <div class="column">
-                        <p id="RPPSNum">10100169461</p>
-                        <p class="indications">Numéro RPPS</p>
-                    </div>
+                <div class="column">
+                    <p id="RPPSNum">10100169461</p>
+                    <p class="indications">Numéro RPPS</p>
                 </div>
             </div>
-
-            <div id="patient">
-                <h3>Le patient</h3>
-                <div class="prescription-box information">
-                    <div class="column">
-                        <input id="patientName" type="text" name=""  required>
-                        <p class="indications" style="margin-bottom:2vh;">NOM *</p>
-                        <input id="patientFirstName" type="text" name="" required>
-                        <p class="indications">Prénom *</p>
-                    </div>
-
-                    <div class="column">
-                        <input id="patientAge" type="number" min="0" max="150" name="">
-                        <p class="indications" style="margin-bottom:2vh;">Âge (ans)</p>
-                        <select id="sexe" name="sexe">
-                            <option value="" disabled selected hidden></option>
-                            <option value="Homme">Homme</option>
-                            <option value="Femme">Femme</option>
-                            <option value="Autre">Autre</option>
-                        </select>
-                        <p class="indications">Sexe</p>
-                    </div>
-
-                    <div class="column">
-                        <input id="patientWeight" type="number" min="0" max="1000" name="">
-                        <p class="indications" style="margin-bottom:2vh;">Poids (kg)</p>
-                        <input id="patientHeight" type="number" min="0" max="300" name="">
-                        <p class="indications">Taille (cm)</p>
-                    </div>
-                </div>
-            </div>
-            <div id="consultation">
-                <h3>La consultation</h3>
-                <div class="information">
-                    <div class="column">
-                        <p id="prescriptionDate">02/05/2023</p>
-                        <p class="indications">Date de l'ordonnance (JJ/MM/AAAA)</p>
-                    </div>
-
-                    <div class="column">
-                        <p id="addressPrescription">1, rue de la République</p>
-                        <p class="indications">Adresse du cabinet</p>
-                    </div>
-
-                    <div class="column">
-                        <p id="consultationPhoneNumber">0454234454</p>
-                        <p class="indications">Tel. cabinet</p>
-                    </div>
-                </div>
-            </div>
-            <div id="prescription">
-                <h3>La prescription</h3>
-
-                <div class="column" >
-                <input id="validityPrescriptionDays" type="text" name="" placeholder="90">
-                <p class="indications">La validité de l'ordonnance (jours)</p>
-                </div>
-
-                <table id="tablePrescription" class="my-table">
-                <thead>
-                    <tr>
-                        <th>Médicament / acte médical *</th>
-                        <th>Posologie *</th>
-                        <th>Durée du traitement *</th>
-                        <th>Renouvellement *</th>
-                        <th>NR</th>
-                        <th>Indications</th>
-                        <th>Supprimer</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            <input type="text" id="medicineAct" list="medicineActDatalist"/>
-                            <datalist id="medicineActDatalist">
-                                
-                            </datalist>
-
-                        </td>
-                        <td>
-                            <input id="posology" type="text" name="" value="30 mg" required>
-                        </td>
-
-                        <td>
-                            <input id="treatmentPeriod" type="text" name="" value="1 mois" required>
-                        </td>
-
-                        <td>
-                            <input id="renewal" type="number" name="" value="3" required>
-                        </td>
-
-                        <td>
-                            <input id="refundable" type="checkbox" name="">
-                        </td>
-
-                        <td>
-                            <textarea id="indication" rows="4" style="word-wrap: break-word;" name="">Prendre le traitement pendant le déjeuner</textarea>
-                        </td>                    
-                        
-                        <td>
-                        <button class="buttonTable" type="delete">
-                            <img src="../assets/delete.png" alt="button delete prescription" />
-                        </button>
-                        </td>
-                    </tr>
-                </tbody>
-                </table>
-                <button class="buttonTable" type="submit">
-                    <img src="../assets/plus.png" alt="button add prescription" />
-                </button>
-            </div>
-            <button id="generatePdfButton" class="ordonnance">Générer l'ordonnance</button>
         </div>
-    </body>
+
+        <div id="patient">
+            <h3>Le patient</h3>
+            <div class="prescription-box information">
+                <div class="column">
+                    <input id="patientName" type="text" name=""  required>
+                    <p class="indications" style="margin-bottom:2vh;">NOM *</p>
+                    <input id="patientFirstName" type="text" name="" required>
+                    <p class="indications">Prénom *</p>
+                </div>
+
+                <div class="column">
+                    <input id="patientAge" type="number" min="0" max="150" name="">
+                    <p class="indications" style="margin-bottom:2vh;">Âge (ans)</p>
+                    <select id="sexe" name="sexe">
+                        <option value="" disabled selected hidden></option>
+                        <option value="Homme">Homme</option>
+                        <option value="Femme">Femme</option>
+                        <option value="Autre">Autre</option>
+                    </select>
+                    <p class="indications">Sexe</p>
+                </div>
+
+                <div class="column">
+                    <input id="patientWeight" type="number" min="0" max="1000" name="">
+                    <p class="indications" style="margin-bottom:2vh;">Poids (kg)</p>
+                    <input id="patientHeight" type="number" min="0" max="300" name="">
+                    <p class="indications">Taille (cm)</p>
+                </div>
+            </div>
+        </div>
+        <div id="consultation">
+            <h3>La consultation</h3>
+            <div class="information">
+                <div class="column">
+                    <p id="prescriptionDate">02/05/2023</p>
+                    <p class="indications">Date de l'ordonnance (JJ/MM/AAAA)</p>
+                </div>
+
+                <div class="column">
+                    <p id="addressPrescription">1, rue de la République</p>
+                    <p class="indications">Adresse du cabinet</p>
+                </div>
+
+                <div class="column">
+                    <p id="consultationPhoneNumber">0454234454</p>
+                    <p class="indications">Tel. cabinet</p>
+                </div>
+            </div>
+        </div>
+        <div id="prescription">
+            <h3>La prescription</h3>
+
+            <div class="column" >
+            <input id="validityPrescriptionDays" type="text" name="" placeholder="90">
+            <p class="indications">La validité de l'ordonnance (jours)</p>
+            </div>
+
+            <table id="tablePrescription" class="my-table">
+            <thead>
+                <tr>
+                    <th>Médicament / acte médical *</th>
+                    <th>Posologie *</th>
+                    <th>Durée du traitement *</th>
+                    <th>Renouvellement *</th>
+                    <th>NR</th>
+                    <th>Indications</th>
+                    <th>Supprimer</th>
+                </tr>
+            </thead>
+            <tbody>
+                <Medicament/>
+            </tbody>
+            </table>
+            <button class="buttonTable" type="submit">
+                <img src="../assets/plus.png" alt="button add prescription" />
+            </button>
+        </div>
+        <button id="generatePdfButton" class="ordonnance">Générer l'ordonnance</button>
+    </div>
 </template>
 
 
 <script>
+    import Medicament from './Medicament.vue';
 
     export default {
-        name: 'DoctorPrescription',
+        name: 'DoctorPrescriptionComponent',
+        components: {
+            Medicament
+        },
         props: {}
     }
 
@@ -247,8 +216,7 @@
 </script>
 
 
-<style scoped>
-
+<style>
 .navigation-bar {
     width: 100%;  /* i'm assuming full width */
     height: 80px; /* change it to desired width */
