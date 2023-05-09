719f79bdea8a538c1baa7ca6bc9322e4cbc8f174
Xynaanthrok <korantin.aime@insa-lyon.fr>
2023-05-09 17:48:18 +0200

    💄 Embellished elements with CSS

--- frontend/src/components/Admin.vue
+++ frontend/src/components/Admin.vue
@@ -1,7 +1,21 @@
 <template>
     <NavigationBar/>
-    <button id="doctorsButton">Médecins</button>
-    <button id="pharmacistsButton">Pharmaciens</button>
+
+    <div class="searchnav">
+        <div class="toggle">
+            <input type="radio" id="doctors" class="job" name="jobView" checked/>
+            <label for="doctors">Médecins</label>
+            <input type="radio" id="pharmacists" class="job" name="jobView"/>
+            <label for="pharmacists">Pharmaciens</label>
+        </div>
+
+        <div class="searchbar">
+            <input type="text" placeholder="Rechercher un professionnel" />
+            <button class="buttonTable" type="submit">
+                <img src="../assets/search.png" id="searchIcon" alt="button search" />
+            </button>
+        </div>
+    </div>
 
     <table class="my-table">
         <thead>
@@ -92,6 +106,10 @@
         width:20vh;
     }
 
+    input:focus {
+        outline: none;
+    }
+
     .buttonTable {
         border:none;
         background-color:transparent;
@@ -101,21 +119,78 @@
         width:2vw;
     }
 
-    #doctorsButton
-    {
-        background-color: rgba(24,23,186,0.63); 
+    .searchnav {
+        display: flex;
+        justify-content: space-between;
+        margin-top: 20px;
     }
 
-    #doctorsButton, #pharmacistsButton
-    {
-        border: 1px solid #000000;
-        border-radius: 15px;
-        height : 5vh;
-        width : 10vw;
-        margin-top : 2vh;
-        margin-left: 5vh;
+    .toggle {
+        display: flex;
+        justify-content: center;
+        margin-left: 39px;
     }
 
-    
+    input[type="radio"].job {
+        display: none;
+		cursor: pointer;
+		outline: none;
+        transition: all .3s ease;
+    }
+
+    input[type="radio"].job + label {
+        position: relative;
+        text-align: center;
+        font-size: 20px;
+        font-variant: small-caps;
+        background: rgba(24,23,186,0.05);
+        border: none;
+        width: 150px;
+        height: 40px;
+        line-height: 40px;
+        transition: all .3s ease;
+    }
+
+    #doctors + label {
+        border-left: 2px solid rgba(24,23,186,0.63);
+        border-top: 2px solid rgba(24,23,186,0.63);
+        border-bottom: 2px solid rgba(24,23,186,0.63);
+    }
+
+    #pharmacists + label {
+        border-right: 2px solid rgba(24,23,186,0.63);
+        border-top: 2px solid rgba(24,23,186,0.63);
+        border-bottom: 2px solid rgba(24,23,186,0.63);
+    }
+
+    input[type="radio"].job:checked + label {
+        background: rgba(24,23,186,0.63);
+        background-clip: padding-box;
+        color: white;
+    }
+
+    .searchbar {
+        display: flex;
+        align-items: center;
+        margin-right: 10vw;
+        border: 2px solid rgba(24,23,186,0.63);
+        border-radius: 20px;
+    }
+
+    .searchbar > input {
+        margin: 0px;
+        padding-left: 15px;
+        width: 500px;
+        height: 30px;
+        border: none;
+        background: transparent;
+        font-size: 16px;
+    }
+
+    #searchIcon {
+        width: 25px;
+        height: 25px;
+        margin: 0 7px 0 7px;
+    }
     
 </style>
